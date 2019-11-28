9f76189907b0acefeb64f8b7f763413659eec9ef
James Leung <jleung128hk@gmail.com>
2019-11-27 23:12:41 -0500

    Tried using data-binding and custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = "James";
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Server info goes here'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {blueprintName: string, blueprintContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent
+    });
+  }
 }
