72abe1e22123ebaf4bf28070410f7a3b3c0819c7
James Leung <jleung128hk@gmail.com>
2019-12-04 21:11:59 -0500

    Added angular form

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-forms',
-  templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.css']
+  selector: "app-forms",
+  templateUrl: "./forms.component.html",
+  styleUrls: ["./forms.component.css"]
 })
 export class FormsComponent {
+  @ViewChild("f") signUpForm: NgForm;
+  defaultQuestion = "pet";
+  answer = "";
+  genders = ["male", "female"];
+
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(this.signUpForm);
   }
 }
