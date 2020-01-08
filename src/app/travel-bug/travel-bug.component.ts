fa3e71d8b9685b06aafe76b1cac9fa4e365d9157
James Leung <pl152@duke.edu>
2020-01-08 09:16:59 -0500

    Added bootstrap cards

--- src/app/travel-bug/travel-bug.component.ts
+++ src/app/travel-bug/travel-bug.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-travel-bug',
-  templateUrl: './travel-bug.component.html',
-  styleUrls: ['./travel-bug.component.css']
+  selector: "app-travel-bug",
+  templateUrl: "./travel-bug.component.html",
+  styleUrls: ["./travel-bug.component.css"]
 })
 export class TravelBugComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onClick() {
+    console.log("You clicked to add a trip card.");
   }
-
 }
--- src/app/travel-bug/travel-bug.module.ts
+++ src/app/travel-bug/travel-bug.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TravelBugComponent } from "./travel-bug.component";
-import { GreetComponent } from './greet/greet.component';
-import { HeaderComponent } from './header/header.component';
+import { GreetComponent } from "./greet/greet.component";
+import { HeaderComponent } from "./header/header.component";
+import { Routes, RouterModule } from "@angular/router";
+import { TripCardComponent } from './trip-card/trip-card.component';
+import { TripFormComponent } from './trip-form/trip-form.component';
+
+const appRoutes: Routes = [{ path: "asdf", component: HeaderComponent }];
 
 @NgModule({
-  declarations: [TravelBugComponent, GreetComponent, HeaderComponent],
-  imports: [CommonModule],
-  exports: [TravelBugComponent]
+  declarations: [TravelBugComponent, GreetComponent, HeaderComponent, TripCardComponent, TripFormComponent],
+  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
+  exports: [TravelBugComponent, RouterModule]
 })
 export class TravelBugModule {}
